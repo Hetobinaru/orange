a681ddc84c4ba23e57553d8205764dfaeb110948
Hetobinaru <hetobinaru@gmail.com>
2022-11-21 17:41:33 +0300

    Automapping issue

--- Prosdtotype.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="contstruct" tilewidth="32" tileheight="32" tilecount="25" columns="5">
- <image source="Prototype.png" width="160" height="160"/>
-</tileset>
--- construct.tsx
+++ construct.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="contstruct" tilewidth="32" tileheight="32" tilecount="25" columns="5">
- <image source="Prototype.png" width="160" height="160"/>
+ <image source="construct.png" width="160" height="160"/>
  <tile id="0">
   <properties>
    <property name="MatchType" value="NonEmpty"/>
